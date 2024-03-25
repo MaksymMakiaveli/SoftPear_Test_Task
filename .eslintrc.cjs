f772b1e49cf558c2bf9f9c39afb988d2975a71e4
Maksym Babii <m.babii@lampalampa.com>
2024-03-25 15:09:58 +0200

    eslint has fixed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,22 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'plugin:react-hooks/recommended',
-    "airbnb-typescript",
-    "prettier"
-  ],
-  plugins: ["prettier"],
+  extends: ['plugin:react-hooks/recommended', 'airbnb-typescript', 'prettier'],
+  plugins: ['prettier'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/jsx-filename-extension': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    '@typescript-eslint/naming-convention': 'off',
   },
 };
